9c2091193e39b64656b44f310dc74f22d0f37af9
Taranchuk <nfhfyxer@gmail.com>
2021-12-05 14:56:23 +0200

    base source project

---
+++ 1.3/Source/VFEPirates/VFEPirates/Class1.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace VFEPirates
+{
+    public class VFEPiratesMod : Mod
+    {
+        public static Harmony harmony;
+        public VFEPiratesMod(ModContentPack modContentPack) : base(modContentPack)
+        {
+            harmony = new Harmony("VFEPirates.Mod");
+            harmony.PatchAll();
+        }
+    }
+}

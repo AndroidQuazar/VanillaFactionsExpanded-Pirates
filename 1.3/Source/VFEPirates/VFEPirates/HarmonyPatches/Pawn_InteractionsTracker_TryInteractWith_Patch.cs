6cf92054733a70733a3946ec7f6e13dc7befb8d0
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-12-06 10:57:35 +0100

    Meme and Precept

---
+++ 1.3/Source/VFEPirates/VFEPirates/HarmonyPatches/Pawn_InteractionsTracker_TryInteractWith_Patch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+
+namespace VFEPirates
+{
+
+    [HarmonyPatch(typeof(Pawn_InteractionsTracker))]
+    [HarmonyPatch("TryInteractWith_Patch")]
+    public static class VFEPirates_Pawn_InteractionsTracker_TryInteractWith_Patch
+    {
+        [HarmonyPostfix]
+        static void AddInteractionThoughts(Pawn __pawn, bool __result)
+        {
+            if (__result && __pawn?.Ideo?.HasPrecept(VFEP_DefOf.VFEP_Camaraderie_Respected) == true )
+            {
+                __pawn.needs?.mood?.thoughts?.memories?.TryGainMemory(VFEP_DefOf.VFEP_Camaraderie_Respected_Memory, null, __pawn?.Ideo?.GetPrecept(VFEP_DefOf.VFEP_Camaraderie_Respected));
+
+            }
+            if (__result && __pawn?.Ideo?.HasPrecept(VFEP_DefOf.VFEP_Camaraderie_Exalted) == true)
+            {
+                __pawn.needs?.mood?.thoughts?.memories?.TryGainMemory(VFEP_DefOf.VFEP_Camaraderie_Exalted_Memory, null, __pawn?.Ideo?.GetPrecept(VFEP_DefOf.VFEP_Camaraderie_Exalted));
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+}
---
+++ 1.3/Source/VFEPirates/VFEPirates/HarmonyPatches/TaleUtility_Notify_PawnDied_Patch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VFEPirates
+{
+
+    [HarmonyPatch(typeof(TaleUtility))]
+    [HarmonyPatch("Notify_PawnDied")]
+    public static class VFEPirates_TaleUtility_Notify_PawnDied_Patch
+    {
+        [HarmonyPostfix]
+        static void NotifyCrewmanDied(Pawn victim)
+        {
+
+            if (victim?.RaceProps?.Humanlike == true && victim.Faction == Faction.OfPlayer)
+            {
+                Find.HistoryEventsManager.RecordEvent(new HistoryEvent(VFEP_DefOf.VFEP_CrewmanDied, new SignalArgs(victim.Named(HistoryEventArgsNames.Victim))), true);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+

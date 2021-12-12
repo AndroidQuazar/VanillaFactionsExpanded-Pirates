5f3c5be7284bd3baf7c78a9d4b381adee0afd727
KylianB <kylian.balde@gmail.com>
2021-12-12 16:38:57 +0100

    Scenario

---
+++ 1.3/Source/VFEPirates/VFEPirates/Buildings/Chunks/FilthSpawner.cs
@@ -0,0 +1,61 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace VFEPirates
+{
+    public class CompProperties_FilthSpawner : CompProperties
+    {
+        public ThingDef filthDef;
+        public int spawnCountOnSpawn = 1;
+        public int spawnRadius = 3;
+        public int totalFilthSpawn = 40;
+        public int spawnEachXticks = 300;
+
+        public CompProperties_FilthSpawner() => this.compClass = typeof(CompFilthSpawner);
+    }
+
+    internal class CompFilthSpawner : ThingComp
+    {
+        int ticksCount = 0;
+        int leftToSpawn;
+
+        private CompProperties_FilthSpawner Props => (CompProperties_FilthSpawner)this.props;
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref ticksCount, "ticksCount", 0);
+            Scribe_Values.Look(ref leftToSpawn, "leftToSpawn", Props.totalFilthSpawn);
+        }
+
+        public override void PostPostMake()
+        {
+            base.PostPostMake();
+            leftToSpawn = Props.totalFilthSpawn;
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (ticksCount % Props.spawnEachXticks == 0 && parent.Spawned)
+            {
+                for (int i = 0; i < Props.spawnCountOnSpawn && leftToSpawn > 0; i++)
+                {
+                    if (RCellFinder.TryFindRandomCellNearWith(parent.Position, c => c.Walkable(parent.Map) && FilthMaker.CanMakeFilth(c, parent.Map, Props.filthDef), parent.Map, out IntVec3 c, 2, Props.spawnRadius) &&
+                        FilthMaker.TryMakeFilth(c, parent.Map, Props.filthDef, 1))
+                    {
+                        leftToSpawn--;
+                    }
+                }
+
+                if (leftToSpawn == 0) parent.AllComps.Remove(this);
+            }
+            ticksCount++;
+        }
+    }
+}
---
+++ 1.3/Source/VFEPirates/VFEPirates/Buildings/Chunks/VolatileEngine.cs
@@ -0,0 +1,87 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.Sound;
+
+namespace VFEPirates
+{
+    public class CompProperties_VolatileEngine : CompProperties
+    {
+        public SoundDef soundAmbient;
+
+        public CompProperties_VolatileEngine() => this.compClass = typeof(CompVolatileEngine);
+    }
+
+    internal class CompVolatileEngine : ThingComp
+    {
+        protected CompGlower compGlower;
+        private Sustainer sustainerProducingPower;
+        int ticksCount = 1;
+        int totalCount = 0;
+        int glowRad = 0;
+        float volumeFactor = 0.1f;
+
+        private CompProperties_VolatileEngine Props => (CompProperties_VolatileEngine)this.props;
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref ticksCount, "ticksCount", 1);
+            Scribe_Values.Look(ref totalCount, "totalCount", 0);
+            Scribe_Values.Look(ref glowRad, "glowRad", 0);
+            Scribe_Values.Look(ref volumeFactor, "volumeFactor", 0.1f);
+        }
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            compGlower = parent.GetComp<CompGlower>();
+            compGlower.Props.glowRadius = glowRad;
+            compGlower.UpdateLit(parent.Map);
+            parent.Map.glowGrid.MarkGlowGridDirty(parent.Position);
+        }
+
+        public override void PostDeSpawn(Map map)
+        {
+            base.PostDeSpawn(map);
+            if (sustainerProducingPower == null || sustainerProducingPower.Ended)
+                return;
+            sustainerProducingPower.End();
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (parent.Spawned)
+            {
+                if (this.Props.soundAmbient != null)
+                {
+                    if (sustainerProducingPower == null || sustainerProducingPower.Ended) sustainerProducingPower = Props.soundAmbient.TrySpawnSustainer(SoundInfo.InMap((TargetInfo)parent));
+                    sustainerProducingPower.Maintain();
+                    sustainerProducingPower.info.volumeFactor = volumeFactor;
+                }
+
+                if (ticksCount % 2500 == 0)
+                {
+                    glowRad++;
+                    compGlower.Props.glowRadius = glowRad;
+                    compGlower.UpdateLit(parent.Map);
+                    parent.Map.glowGrid.MarkGlowGridDirty(parent.Position);
+                    volumeFactor += 0.05f;
+                    sustainerProducingPower.End();
+                    totalCount++;
+                }
+                ticksCount++;
+
+                if (totalCount == 6)
+                {
+                    parent.GetComp<CompExplosive>().StartWick();
+                }
+            }
+        }
+    }
+}

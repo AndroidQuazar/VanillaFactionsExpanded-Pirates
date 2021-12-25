dc4d324545946fc6da827ed5bdb866ebb590fde6
Phil <supersmashphil@gmail.com>
2021-12-25 00:16:36 -0700

    eye patch curse

---
+++ 1.3/Source/VFEPirates/VFEPirates/Curses/CurseOfTheEyePatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace VFEPirates
+{
+    public class CurseOfTheEyePatch : CurseWorker
+    {
+        public override void DoPatches(Harmony harmony)
+        {
+            harmony.Patch(original: AccessTools.Method(typeof(ShotReport), nameof(ShotReport.HitFactorFromShooter), parameters: new Type[] { typeof(float), typeof(float) }), 
+                prefix: new HarmonyMethod(AccessTools.Method(typeof(CurseOfTheEyePatch), nameof(HalfAccuracy))));
+        }
+
+        public static void HalfAccuracy(ref float accRating, float distance)
+        {
+            if (IsActive(typeof(CurseOfTheEyePatch)))
+			{
+                accRating /= 2;
+            }
+        }
+    }
+}

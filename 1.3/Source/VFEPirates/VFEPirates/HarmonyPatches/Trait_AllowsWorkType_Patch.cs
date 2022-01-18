0ebf816a3e1cc41093f08d654cce5c5dfa8d2b58
Taranchuk <nfhfyxer@gmail.com>
2022-01-18 18:53:41 +0200

    allow warcasket pawns to haul

---
+++ 1.3/Source/VFEPirates/VFEPirates/HarmonyPatches/Trait_AllowsWorkType_Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VFEPirates
+{
+    [HarmonyPatch(typeof(Trait), "AllowsWorkType")]
+    public static class Trait_AllowsWorkType_Patch
+    {
+        public static void Postfix(Trait __instance, WorkTypeDef workDef, ref bool __result)
+        {
+            if (__instance.def == VFEP_DefOf.VFEP_WarcasketTrait && workDef == WorkTypeDefOf.Hauling)
+            {
+                __result = true;
+            }
+        }
+    }
+}

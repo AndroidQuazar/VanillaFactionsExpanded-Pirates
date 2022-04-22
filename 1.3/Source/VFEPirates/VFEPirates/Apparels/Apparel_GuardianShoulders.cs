98a9041a6551f5695caa76b4208e885ed2569802
Taranchuk <nfhfyxer@gmail.com>
2022-04-22 22:15:44 +0300

    added guardian code

---
+++ 1.3/Source/VFEPirates/VFEPirates/Apparels/Apparel_GuardianShoulders.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+
+namespace VFEPirates
+{
+    public class Apparel_GuardianShoulders : Apparel_Warcasket
+    {
+        public override bool CheckPreAbsorbDamage(DamageInfo dinfo)
+        {
+            if (Rand.Chance(0.25f))
+            {
+                return true;
+            }
+            else
+            {
+                return base.CheckPreAbsorbDamage(dinfo);
+            }
+        }
+    }
+}

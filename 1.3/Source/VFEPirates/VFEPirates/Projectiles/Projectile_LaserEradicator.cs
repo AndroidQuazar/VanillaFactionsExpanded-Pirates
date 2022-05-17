336804f599b959220b0efff8137952065244b22a
Taranchuk <nfhfyxer@gmail.com>
2022-05-17 20:43:32 +0300

    pushed projectile laser eradicator class

---
+++ 1.3/Source/VFEPirates/VFEPirates/Projectiles/Projectile_LaserEradicator.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using VanillaWeaponsExpandedLaser;
+using Verse;
+
+namespace VFEPirates
+{
+    public class Projectile_LaserEradicator : LaserBeam
+    {
+        public override void Impact(Thing hitThing)
+        {
+            GenExplosion.DoExplosion(this.Position, this.Map, 1.9f, DamageDefOf.Bomb, this.launcher);
+            base.Impact(hitThing);
+        }
+    }
+}
